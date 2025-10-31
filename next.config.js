684e0a45c4ac73463bd1201491063be273052eb7
David Reynolds <reynolds@Davids-MacBook-Pro.local>
2025-10-31 11:36:25 +0000

    Rename next.config.ts to .js for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+  },
+};
+
 module.exports = nextConfig;
